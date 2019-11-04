2103cfd0ce350b919ddaa8d763e2f8ebf6c1a39b
jeffgrunewald <jeff@grunewalddesign.com>
2019-11-04 14:10:21 -0500

    repo scaffold

---
+++ lib/kitt.ex
@@ -0,0 +1,18 @@
+defmodule Kitt do
+  @moduledoc """
+  Documentation for Kitt.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Kitt.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
