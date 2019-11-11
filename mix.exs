c9ec8484118f261c25e528729b36b8a570ab3438
jeffgrunewald <jeff@grunewalddesign.com>
2019-11-11 10:49:10 -0500

    Updating deps, committing asn1 compiler code

--- mix.exs
+++ mix.exs
@@ -1,28 +1,51 @@
 defmodule Kitt.MixProject do
   use Mix.Project
 
-  def project do
+  def project() do
     [
       app: :kitt,
       version: "0.1.0",
-      elixir: "~> 1.9",
+      elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      docs: docs(),
+      package: package(),
+      description: description(),
+      source_url: "https://github.com/jeffgrunewald/kitt"
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
-  def application do
+  def application() do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
-  defp deps do
+  defp deps() do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:credo, "~> 1.1", only: :dev, runtime: false},
+      {:dialyxir, "~> 1.0.0-rc.7", only: :dev, runtime: false},
+      {:jason, "~> 1.1"}
+    ]
+  end
+
+  defp description(), do: "Encoder and decoder for DSRC messages defined by the J2735 standard."
+
+  defp package() do
+    [
+      maintainers: ["jeffgrunewald"],
+      licenses: ["Apache 2.0"],
+      links: %{:GitHub => "https://github.com/jeffgrunewald/kitt"}
+    ]
+  end
+
+  defp docs() do
+    [
+      main: "readme",
+      source_url: "https://github.com/jeffgrunewald/kitt",
+      extras: [
+        "README.md"
+      ]
     ]
   end
 end
